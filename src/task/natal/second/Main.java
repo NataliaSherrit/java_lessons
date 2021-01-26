2c5801572ee3cb71d1da859131daad0f86fd6533
NataliaSherrit <natalia.sherrit@gmail.com>
2021-01-26 21:38:16 +0300

    переменной целочисленного типа присвоено наибольшее возможное значение значение

--- src/task/natal/second/Main.java
+++ src/task/natal/second/Main.java
@@ -1,8 +1,8 @@
 package task.natal.second;
 
 public class Main {
-    static double d  = 17.567;
-    static byte b = 13;
+    static double d = 17.567;
+    static byte b = 127;
     public static void main(String[] args) {
 
     }
