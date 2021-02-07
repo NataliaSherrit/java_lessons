f038b98e7d1d43cdec070f94b8a7a4da0b068189
NataliaSherrit <natalia.sherrit@gmail.com>
2021-02-07 13:28:40 +0300

    задача 4, операторы (ввод-вывод, условие)

---
+++ src/task/natal/task4/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите число х");
+        Scanner x = new Scanner(System.in);
+        double num1 = x.nextInt();
+        System.out.println("Введите число у");
+        Scanner y = new Scanner(System.in);
+        double num2 = y.nextInt();
+        System.out.println("Введите число z");
+        Scanner z = new Scanner(System.in);
+        double num3 = z.nextInt();
+        double a = (num1+num2+num3)/3;
+        System.out.println("среднее арифметическое равно " + a);
+        int b = (int) (a/3);
+        if (b>3){
+            System.out.println("Программа выполнена корректно");
+        }
+
+
+    }
+}
