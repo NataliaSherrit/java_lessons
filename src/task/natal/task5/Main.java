51bc929d0f9f3a883258da3c4cc8d927e5305735
NataliaSherrit <natalia.sherrit@gmail.com>
2021-02-07 21:25:20 +0300

    правка по задаче 5

---
+++ src/task/natal/task5/Main.java
@@ -0,0 +1,32 @@
+package task.natal.task5;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите число х");
+        Scanner x = new Scanner(System.in);
+        double num1 = x.nextDouble();
+        System.out.println("Введите число у");
+        Scanner y = new Scanner(System.in);
+        double num2 = y.nextDouble();
+        System.out.println("Введите один символ действия '+', '-', '/', '*'");
+        Scanner z = new Scanner(System.in);
+        String num3 = z.nextLine();
+            switch (num3) {
+                case "+":
+                    System.out.println(num1 + num2);
+                    break;
+                case "-":
+                    System.out.println(num1 - num2);
+                    break;
+                case "/":
+                    if (num2 == 0.00) {
+                        System.out.println("На ноль делить нельзя");
+                    } else {
+                        System.out.println(num1 / num2);
+                    }
+                    break;
+                case "*":
+                    System.out.println(num1 * num2);
+                    break;
+            }
+        }}
